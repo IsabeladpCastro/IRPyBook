90ed46a87833ed82ee351f113bf8012da6d62c42
Isabela Castro <isabeladpcastro@gmail.com>
2023-12-19 14:06:01 -0300

    Configuração e autenticação de usuarios - Inicio do BackEnd

--- irpybooksite/irpybookapp/urls.py
+++ irpybooksite/irpybookapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("index/", views.index, name="index"),
-    path("", views.login, name="login"),
+    path("", views.login_view, name="login"),
     path("home/", views.mainPage, name="home"),
     path("register/", views.register, name="register"),
     path("registerBook/", views.registerBook, name="registerBook"),
--- irpybooksite/irpybookapp/views.py
+++ irpybooksite/irpybookapp/views.py
@@ -1,18 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Nome de usuario ou senha incorretos.')
+            
+    return render(request, 'login.html')
+        
+        
 def mainPage(request):
     return render(request, 'home.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registro Bem-Sucedido')
+            return redirect('home')
+        else:
+            messages.error(request, 'Erro no Registro')
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'register.html', {'form': form})
 
 def registerBook(request):
     return render(request, 'registerBook.html')
