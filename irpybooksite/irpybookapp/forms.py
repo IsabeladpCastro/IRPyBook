2206d3e946cd3daefbed0d73331f58915c010efb
Rafael Silva <nightmarezlolzin@gmail.com>
2023-12-22 13:39:17 -0300

    Ajustes do design da pagina de registro

--- irpybooksite/irpybookapp/forms.py
+++ irpybooksite/irpybookapp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        labels = {
+            'username': '',  # Define rótulo como vazio para o campo username
+            'password1': '',  # Define rótulo como vazio para o campo password1
+            'password2': '',  # Define rótulo como vazio para o campo password2
+        }
+
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Digite um email valido')
     
