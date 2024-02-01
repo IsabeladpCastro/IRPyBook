e3e43f7ed5e9cde44bb04359626315648436afa0
Rafael Silva <nightmarezlolzin@gmail.com>
2024-01-31 23:38:36 -0300

    Adição do da borda na pagina de

--- irpybooksite/irpybookapp/forms.py
+++ irpybooksite/irpybookapp/forms.py
@@ -7,9 +7,14 @@ from .models import Livro
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         labels = {
-            'username': '',  # Define rótulo como vazio para o campo username
-            'password1': '',  # Define rótulo como vazio para o campo password1
-            'password2': '',  # Define rótulo como vazio para o campo password2
+            'username': '',
+            'password1': '',
+            'password2': '',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Digite seu nome de usuário'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Digite sua senha'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Digite novamente sua senha'}),
         }
 class LivroForm(forms.ModelForm):
     class Meta:
