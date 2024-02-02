a8831cf077b4c9d69b9d9e5c82f0e89ee6061c67
Isabela Castro <isabeladpcastro@gmail.com>
2024-02-02 15:19:18 -0300

    Alteração no arquivo vercel.json e settings

--- irpybooksite/irpybooksite/urls.py
+++ irpybooksite/irpybooksite/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("irpybookapp.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
