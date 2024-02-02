9e80dad90d21c62dfbb756577e780ddb519895ac
Isabela Castro <isabeladpcastro@gmail.com>
2024-02-02 16:28:21 -0300

    Tentativa de deploy 01

--- irpybooksite/irpybooksite/settings.py
+++ irpybooksite/irpybooksite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, '.vercel_build_output', 'static')
